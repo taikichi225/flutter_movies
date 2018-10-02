ca3c43d7b3141b6583627ac19ad94cef4d55ca4d
taiki.kono <taiki.kono@ulsystems.co.jp>
2018-10-02 23:57:03 +0900

    ウィジェットの分割

--- lib/movie_detail.dart
+++ lib/movie_detail.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import './movie_client.dart';
 import './models/movie_detail_model.dart';
 
-
 class MovieDetail extends StatelessWidget {
   final _imdbID;
 
@@ -12,31 +11,18 @@ class MovieDetail extends StatelessWidget {
 
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Detail"),
-      ),
+      appBar: AppBar(title: Text("Detail"),),
       body: FutureBuilder(
         future: _fetchMovie(),
         builder: (BuildContext context, AsyncSnapshot<MovieDetailModel> snapshot) {
-          if(!snapshot.hasData) {
-            return Center(child: CircularProgressIndicator(),);
-          }
+          if(!snapshot.hasData) { return Center(child: CircularProgressIndicator(),); }
 
           MovieDetailModel movie = snapshot.data;
           return Column(
             children: <Widget>[
               Expanded(
                 child: ListView(
-                  children: <Widget>[
-                    Hero(tag:movie.imdbID, child: Image.network(movie.poster, fit: BoxFit.contain,),),
-                    ListTile(title: Text("Title"), subtitle: Text(movie.title),),
-                    ListTile(title: Text("Year"), subtitle: Text(movie.year),),
-                    ListTile(title: Text("Released"), subtitle: Text(movie.released),),
-                    ListTile(title: Text("Runtime"), subtitle: Text(movie.runtime),),
-                    ListTile(title: Text("Genre"), subtitle: Text(movie.genre),),
-                    ListTile(title: Text("Director"), subtitle: Text(movie.director),),
-                    ListTile(title: Text("Actors"), subtitle: Text(movie.actors),),
-                  ],
+                  children: buildListTile(movie),
                 ),
               ),
             ],
@@ -44,6 +30,19 @@ class MovieDetail extends StatelessWidget {
         },
       ),
     );
+  }
+
+  List<Widget> buildListTile(MovieDetailModel movie) {
+    return [
+      Hero(tag:movie.imdbID, child: Image.network(movie.poster, fit: BoxFit.contain,),),
+      ListTile(title: Text("Title"), subtitle: Text(movie.title),),
+      ListTile(title: Text("Year"), subtitle: Text(movie.year),),
+      ListTile(title: Text("Released"), subtitle: Text(movie.released),),
+      ListTile(title: Text("Runtime"), subtitle: Text(movie.runtime),),
+      ListTile(title: Text("Genre"), subtitle: Text(movie.genre),),
+      ListTile(title: Text("Director"), subtitle: Text(movie.director),),
+      ListTile(title: Text("Actors"), subtitle: Text(movie.actors),),
+    ];
   }
 
   Future<MovieDetailModel> _fetchMovie() async {
